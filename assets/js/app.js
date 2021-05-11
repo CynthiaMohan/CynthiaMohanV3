572a25d99f2a7189d6aec8f8b40aace0acfeb269
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-05-10 23:42:54 -0700

    update layout

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,18 @@
 const about = document.querySelector('#aboutMe');
 const work = document.querySelector('#work');
+const linkAboutMe = document.querySelector('#linkAboutMe');
+const linkWork = document.querySelector('#linkWork');
+
+
+linkAboutMe.addEventListener('click', () => {
+    if (about) {
+        work.classList.add("hidden");
+        about.classList.remove("hidden");
+    }
+});
+linkWork.addEventListener('click', () => {
+    if (work) {
+        about.classList.add("hidden");
+        work.classList.remove("hidden");
+    }
+});
