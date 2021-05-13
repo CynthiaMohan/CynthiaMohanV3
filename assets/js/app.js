b55cb0040d30ce79885d8dd28ae454616a85d901
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-05-13 00:36:39 -0700

    media queries <576 pixels

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,23 +2,26 @@ const about = document.querySelector('#aboutMe');
 const work = document.querySelector('#work');
 const linkAboutMe = document.querySelector('#linkAboutMe');
 const linkWork = document.querySelector('#linkWork');
+const mediaQuery = window.matchMedia('(max-width: 576px)');
 
 
 linkAboutMe.addEventListener('click', () => {
     if (about) {
         work.classList.add("hidden");
         about.classList.remove("hidden");
+
     }
 });
 linkWork.addEventListener('click', () => {
     if (work) {
+
         about.classList.add("hidden");
         work.classList.remove("hidden");
     }
 });
 
-const mediaQuery = window.matchMedia('(min-width: 411px)');
 if (mediaQuery.matches) {
     about.classList.remove("hidden");
     work.classList.remove("hidden");
+
 }
